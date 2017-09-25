9fedf04b1cd8b42fa052f7e51d95f4914cf4c153
Nat Welch <nat@natwelch.com>
2017-09-25 17:22:09 +0000

    Add 204 route

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	// "gopkg.in/unrolled/secure.v1"
 )
 
 func main() {
@@ -15,18 +16,20 @@ func main() {
 
 	server := http.NewServeMux()
 	server.HandleFunc("/", hello)
+	server.HandleFunc("/204", twoOhFour)
+
 	log.Printf("Server listening on port %s", port)
 	log.Fatal(http.ListenAndServe(":"+port, server))
 }
 
-type Resp struct {
-	Status  string
-	Message string
+type HelloRespJson struct {
+	Status  string `json:"status"`
+	Message string `json:"msg"`
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
-	resp := Resp{"ok", "Hello World"}
+	resp := HelloRespJson{"ok", "Hello World"}
 
 	js, err := json.Marshal(resp)
 	if err != nil {
@@ -36,4 +39,8 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
+}
+
+func twoOhFour(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
 }
