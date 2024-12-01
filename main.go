01e0feab294cfe3f0bec20c4dade44ee1c793777
Nat Welch <nat@natwelch.com>
2024-12-01 19:49:50 +0000

    whee

--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/go-chi/chi/v5"
+	chi "github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/icco/gutil/logging"
+	"github.com/unrolled/secure"
 )
 
 var (
@@ -24,9 +25,26 @@ func main() {
 	}
 	log.Infow("Starting up", "host", fmt.Sprintf("http://localhost:%s", port))
 
+	secureMiddleware := secure.New(secure.Options{
+		AllowedHosts:          []string{"example\\.com", ".*\\.example\\.com"},
+		AllowedHostsAreRegex:  true,
+		HostsProxyHeaders:     []string{"X-Forwarded-Host"},
+		SSLRedirect:           true,
+		SSLHost:               "ssl.example.com",
+		SSLProxyHeaders:       map[string]string{"X-Forwarded-Proto": "https"},
+		STSSeconds:            31536000,
+		STSIncludeSubdomains:  true,
+		STSPreload:            true,
+		FrameDeny:             true,
+		ContentTypeNosniff:    true,
+		BrowserXssFilter:      true,
+		ContentSecurityPolicy: "script-src $NONCE",
+	})
+
 	r := chi.NewRouter()
 	r.Use(middleware.RealIP)
 	r.Use(logging.Middleware(log.Desugar(), project))
+	r.Use(secureMiddleware.Handler)
 
 	r.Get("/", hello)
 	r.Get("/healthz", hello)
